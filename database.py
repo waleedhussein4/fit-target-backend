bdf35898fb1b011d499533d43679ffc3f69d1c19
Waleed <walid.a.hussein@hotmail.com>
2024-11-29 17:58:55 +0300

    fix url

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_db_app"
-SQLALCHEMY_DATABASE_URL = "postgres://neondb_owner:w6pqxjh2edyA@ep-damp-pine-a293secm-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:w6pqxjh2edyA@ep-damp-pine-a293secm-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
