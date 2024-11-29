bd954314a0e03f776c307450f140464c54833eea
Antoine Medawar <147323652+aem2004@users.noreply.github.com>
2024-11-29 21:42:59 +0300

    sign in/up APIs, user model, crud ops

---
+++ FitTargetBackend/Schemas/userCreate.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    firstName: str
+    lastName: str
+    email: EmailStr
+    password: str
+    age: int
+    gender: str
+    weight: float
+    height: float
+    targetWeight: float
+    weightMeasurementPreference: str
+    targetPeriod: int
