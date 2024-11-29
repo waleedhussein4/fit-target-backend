1ff14f52916e7f14796960f8b74a16efeb1a1941
Waleed <walid.a.hussein@hotmail.com>
2024-11-29 18:00:13 +0300

    fix url

--- database.py
+++ database.py
@@ -5,9 +5,8 @@ from sqlalchemy.orm import sessionmaker
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_db_app"
 SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:w6pqxjh2edyA@ep-damp-pine-a293secm-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
 
-engine = create_engine(
-  SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
