ab169023e0828689111d7ec11d8e8eb5ab3162d8
jadghazi <jadghazi22@gmail.com>
2024-12-07 19:09:36 +0200

    Update user

--- Schemas/userCreate.py
+++ Schemas/userCreate.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class UserCreate(BaseModel):
     firstName: str
@@ -16,3 +17,19 @@ class UserCreate(BaseModel):
 class UserSignIn(BaseModel):
     email: EmailStr
     password: str
+
+class UserUpdate(BaseModel):
+    firstName: Optional[str]
+    lastName: Optional[str]
+    email: Optional[EmailStr]
+    password: Optional[str]
+    age: Optional[int]
+    gender: Optional[str]
+    weight: Optional[float]
+    height: Optional[float]
+    targetWeight: Optional[float]
+    weightMeasurementPreference: Optional[str]
+    targetPeriod: Optional[int]
+
+    class Config:
+        orm_mode = True
