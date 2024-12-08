6f560640b2c9b02ecda6d38ff91b0b8f46f5fb7a
Waleed <walid.a.hussein@hotmail.com>
2024-12-08 16:13:57 +0200

    fix Schema checksync

--- Schemas/sync.py
+++ Schemas/sync.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Any
 
 class CheckSync(BaseModel):
-    user_email: EmailStr
-    workouts_pending_upload: List[int]  # Local workout IDs pending upload
-    food_entries_pending_upload: List[Any]  # Local food entry identifiers pending upload
-    last_local_sync: str  # ISO 8601 formatted string for the last sync timestamp
+    user_id: int
+    workoutsPendingUpload: List[int]  # Local workout IDs pending upload
+    foodEntriesPendingUpload: List[Any]  # Local food entry identifiers pending upload
+    lastLocalSync: str  # ISO 8601 formatted string for the last sync timestamp
