44d366c1fd2127ccdcf6de78a251258f7b28fbdf
Waleed <walid.a.hussein@hotmail.com>
2024-12-09 02:37:51 +0200

    test

--- Models/workoutModel.py
+++ Models/workoutModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey, BigInteger
 from database import Base
 
 class Workout(Base):
